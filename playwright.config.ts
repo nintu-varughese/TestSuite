60ff21e00956260a661a62c037deccd34351ddec
nintu-varughese <nintu.varughese@testingmavens.com>
2025-10-28 11:30:42 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,6 +59,14 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+      {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     // Other browsers can be enabled if needed
   ],
